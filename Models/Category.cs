2c5cacd4a0df957ae4689dc8f2e179f503b1b659
Stella van der burg <estellavanderburg@gmail.com>
2020-10-05 10:44:59 +0200

    - Added Category Model - Added Item Model - Added AppDbContext (In progress)

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItemCatalogue.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public Collection<Item> Items{ get; set; }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItemCatalogue.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public ItemQuality Quality { get; set; }
+        public decimal BasePrice { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public Collection<Category> Categories { get; set; }
+    }
+}
+
+public enum ItemQuality
+{
+    Basic,
+    Nice,
+    Good,
+    Excellent,
+    Exceptional,
+    Poor
+}
